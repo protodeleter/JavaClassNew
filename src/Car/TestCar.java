541699055be4aa72625cd044dce98f64397a5139
Pavel <pavel.nsnv@gmail.com>
2025-11-29 21:31:55 +0200

    hw

--- src/Car/TestCar.java
+++ src/Car/TestCar.java
@@ -7,10 +7,35 @@ public class TestCar {
 
         Scanner inp = new Scanner(System.in);
 
+        String carType = "";
+        String color = "";
+        double kms, minKms = 0;
+
+        Car newestCar;
 
         for (int i = 0; i < 30; i++) {
 
+            System.out.println("Enter car type:");
+            carType = inp.nextLine();
 
+
+            System.out.println("Enter car color:");
+            color = inp.nextLine();
+
+            System.out.println("Enter car kilometers:");
+            kms = inp.nextDouble();
+
+
+            if (i == 0) {
+                minKms = kms;
+            }
+
+            if (kms < minKms) {
+                minKms = kms;
+            }
         }
+
+        System.out.println("Car with lowest kms: ");
+
     }
 }
